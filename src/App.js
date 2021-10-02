d09388f16007a740835900e087b590d2cd4888a0
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-02 14:35:31 +0200

    agrego tarea de softskill

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicks,setClicks] = useState(0)
+  const [user,setUser] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <h1>Contador</h1>
+  <p>{clicks}</p>
+  <button onClick={()=>setClicks(clicks+1)}>+</button>
+  <button onClick={()=>clicks>0? setClicks(clicks-1): clicks}>-</button>
+  <h1>API</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Last Name</th>
+        <th>email</th>
+      </tr>
+      </thead>
+      <tbody>
+      {user.map(user=>{
+        return(
+          <tr>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+          </tr>
+        )
+      })}  
+      </tbody>
+  </table>
+  
+  </>);
 }
 
 export default App;
