f91e0d4af37487eea825530809ddd867399ee114
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-03 23:44:01 +0200

    agrego cambios de estilo

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [clicks,setClicks] = useState(0)
-  const [user,setUser] = useState([])
-  
+  const [clicks, setClicks] = useState(0);
+  const [user, setUser] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3000/users")
-    .then(res=>res.json())
-    .then(data=>setUser(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
 
   return (
-  <div className="container">
-    <div>
-  <h1>Counter clicks</h1>
-  <button onClick={()=>clicks>0? setClicks(clicks-1): clicks} type="button" className="btn btn-primary btn-sm">-</button>
-  <input value={clicks}></input>
-  <button onClick={()=>setClicks(clicks+1)} type="button" className="btn btn-primary btn-sm">+</button>
-  </div>
-  <div>
-  <h1>Fake API</h1>
-  
-  <table className="table">
-    <thead className="thead-dark">
-      <tr>
-        <th>Name</th>
-        <th>Last Name</th>
-        <th>email</th>
-      </tr>
-      </thead>
-      <tbody>
-      {user.map(user=>{
-        return(
-          <tr>
-            <td>{user.firstName}</td>
-            <td>{user.lastName}</td>
-            <td>{user.email}</td>
-          </tr>
-        )
-      })}  
-      </tbody>
-  </table>
-  </div>
-  
-  </div>);
+    <div className="container">
+      <div>
+        <h1>Counter clicks</h1>
+        <br />
+        <button
+          onClick={() => (clicks > 0 ? setClicks(clicks - 1) : clicks)}
+          type="button"
+          className="btn btn-primary btn-sm"
+        >
+          -
+        </button>
+        <input value={clicks}></input>
+        <button
+          onClick={() => setClicks(clicks + 1)}
+          type="button"
+          className="btn btn-primary btn-sm"
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <br />
+        <h1>Fake API</h1>
+        <br />
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th>Name</th>
+              <th>Last Name</th>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map((user) => {
+              return (
+                <tr>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
